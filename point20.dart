534283c14219437a2108823618690a82bdff8ec5
Soban Sandhu <sobansandhu2k3@gmail.com>
2024-05-27 12:44:09 +0500

    SQUARE ADDED

--- point20.dart
+++ point20.dart
@@ -4,8 +4,15 @@ import 'dart:core';
 void main() {
   double area = CalculateCircleArea(5);
   print('Area of Circle is $area');
+
+  int? square1 = square(4);
+  print('Square of Number is $square1');
 }
 
 double CalculateCircleArea(double radius) {
   return 3.14 * radius * radius;
+}
+
+int square(int num) {
+  return num * num;
 }
