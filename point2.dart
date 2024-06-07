37273e326a585fe3f8646358fc97a65a72dad930
Soban Sandhu <sobansandhu2k3@gmail.com>
2024-06-07 11:08:26 +0500

    Reverse Numb Added

--- point2.dart
+++ point2.dart
@@ -5,4 +5,8 @@ void main() {
   for (int i = 1; i <= 10; i++) {
     print('numbers are $i');
   }
+  print('\n\nSeperation\n\n');
+  for (int i = 10; i > 0; i--) {
+    print('Numbers in reverse are $i');
+  }
 }
