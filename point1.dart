979781f7835c5848c3f41d452c7e3ed8aedf795a
Soban Sandhu <sobansandhu2k3@gmail.com>
2024-06-25 20:47:43 +0500

    Updated:

--- point1.dart
+++ point1.dart
@@ -1,7 +1,12 @@
 import 'dart:core';
+import 'dart:io';
 //import 'dart:io';
 
 void main() {
   String productName = 'ASP.Net MVC';
   print(productName);
+  // int n = int.parse(stdin.readLineSync()!);
+  // int i = int.parse(stdin.readLineSync()!);
+  String name = 'sandhu family';
+  print(name);
 }
